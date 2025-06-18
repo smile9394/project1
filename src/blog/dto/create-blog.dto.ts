29c19a84872366f4ebec536acc31a5a65aa8981a
smile9394 <ceo@shinjinbd.com>
2025-06-18 17:12:12 +0900

    apply class validator

--- src/blog/dto/create-blog.dto.ts
+++ src/blog/dto/create-blog.dto.ts
@@ -1,15 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateBlogDto {
   @ApiProperty({ example: 'title' })
+  @IsNotEmpty()
+  @IsString()
   title: string;
 
   @ApiProperty({ example: 'description' })
+  @IsNotEmpty()
+  @IsString()
   description: string;
 
   @ApiProperty({ example: 10000 })
+  @IsNotEmpty()
+  @IsNumber()
   price: number;
-
+  //데이터 무결성을 위한 클래스 벨리데이터
   @ApiProperty({ example: 'category' })
+  @IsNotEmpty()
+  @IsString()
   category: string;
 }
