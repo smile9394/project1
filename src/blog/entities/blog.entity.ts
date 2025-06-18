2475f0c3af833d8976bbae54a0ffcfcce512c6a0
smile9394 <ceo@shinjinbd.com>
2025-06-18 12:27:31 +0900

    블로그엔티티생성

--- src/blog/entities/blog.entity.ts
+++ src/blog/entities/blog.entity.ts
@@ -1,1 +1,19 @@
-export class Blog {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Blog {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column()
+  public title: string;
+
+  @Column()
+  public description: string;
+
+  @Column()
+  public price: number;
+
+  @Column()
+  public category: string;
+}
