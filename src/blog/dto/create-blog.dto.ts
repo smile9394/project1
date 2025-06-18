c0902f6f6a95a74cd0fa3ca9286dd031ebb1aa27
smile9394 <ceo@shinjinbd.com>
2025-06-18 17:04:19 +0900

    swagger property examples

--- src/blog/dto/create-blog.dto.ts
+++ src/blog/dto/create-blog.dto.ts
@@ -1,1 +1,15 @@
-export class CreateBlogDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateBlogDto {
+  @ApiProperty({ example: 'title' })
+  title: string;
+
+  @ApiProperty({ example: 'description' })
+  description: string;
+
+  @ApiProperty({ example: 10000 })
+  price: number;
+
+  @ApiProperty({ example: 'category' })
+  category: string;
+}
