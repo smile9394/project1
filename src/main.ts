e6fbfc95638873cb2a6644ea1a5902fee32b18ed
smile9394 <ceo@shinjinbd.com>
2025-06-18 17:03:51 +0900

    swagger set up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Swagger 세팅
+  const config = new DocumentBuilder()
+    .setTitle('sungwoo blog api')
+    .setDescription('it is my blog')
+    .setVersion('1.0')
+    .addTag('sungwoo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
