2eddd3f379b29c12565309342e663d76a496c06a
smile9394 <ceo@shinjinbd.com>
2025-06-19 14:53:56 +0900

    회원가입 api 생성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserService } from '../user/user.service';
+import { CreateUserDto } from '../user/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(private readonly userService: UserService) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  // 회원가입
+  async signinUser(createUserDto: CreateUserDto) {
+    return await this.userService.createUser(createUserDto);
   }
 }
