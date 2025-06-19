073e5ce7ee569797b46b1a9fe8b1a657581ff176
smile9394 <ceo@shinjinbd.com>
2025-06-19 17:38:06 +0900

    검증

---
+++ src/auth/strategies/local-auth.strategy.ts
@@ -0,0 +1,17 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class LocalAuthStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      usernameField: 'email',
+    });
+  }
+
+  async validate(email: string, password: string) {
+    return this.authService.loginUser({ email, password });
+  }
+}
