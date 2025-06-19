e534621bb4f3390fe19c68712e7ae6659b3d5d34
smile9394 <ceo@shinjinbd.com>
2025-06-19 17:48:16 +0900

    가드랑 검증을 활용하는 auth logic2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,12 +6,16 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { LoginUserDto } from '../user/dto/login-user.dto';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { RequestWithUserInterface } from './interfaces/requestWithUser.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -25,7 +29,8 @@ export class AuthController {
 
   // 로그인
   @Post('/login')
-  async loginUser(@Body() loginUserDto: LoginUserDto) {
-    return await this.authService.loginUser(loginUserDto);
+  @UseGuards(LocalAuthGuard)
+  async loginUser(@Req() req: RequestWithUserInterface) {
+    return req.user;
   }
 }
