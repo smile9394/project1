3b5d9e3abed2c2f5f30d71b5f50e9362db6044f5
smile9394 <ceo@shinjinbd.com>
2025-06-19 17:39:26 +0900

    module에서 strategy불러오기

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { EmailModule } from '../email/email.module';
+import { LocalAuthStrategy } from './strategies/local-auth.strategy';
 
 @Module({
   imports: [UserModule, EmailModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalAuthStrategy],
 })
 export class AuthModule {}
